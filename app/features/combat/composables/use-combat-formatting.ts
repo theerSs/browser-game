391323942b5585a1e61db6b91d7b048a4571abe5
Jakub <jakublasek41@gmail.com>
2025-08-23 12:38:02 +0200

    refactor: change minimum block to 5%

--- app/features/combat/composables/use-combat-formatting.ts
+++ app/features/combat/composables/use-combat-formatting.ts
@@ -3,7 +3,7 @@ export function useCombatFormatting(combatState: Ref<CombatState | null>) {
   const enemy = computed(() => combatState.value?.enemy);
 
   const damageRange = computed(() => player.value ? `${player.value.stats.damage[0]} - ${player.value.stats.damage[1]}` : "");
-  const defenceValue = computed(() => player.value ? `${player.value.stats.defence}%` : "");
+  const defenceValue = computed(() => player.value ? `${Math.max(5, player.value.stats.defence)}%` : "");
   const dodgeChance = computed(() => player.value && enemy.value ? `${Math.max(5, player.value.stats.dex - enemy.value.stats.dex)}%` : "");
 
   return { damageRange, defenceValue, dodgeChance };
--- server/features/combat/actions/defend.ts
+++ server/features/combat/actions/defend.ts
@@ -8,7 +8,7 @@ export function doDefend(state: CombatState) {
   const { player, enemy } = state;
 
   const enemyDamage = _.random(enemy.stats.damage[0], enemy.stats.damage[1]);
-  const defenceModifier = player.stats.defence / 100;
+  const defenceModifier = Math.max(0.05, player.stats.defence / 100);
   const modifiedDamage = Math.floor(enemyDamage - (enemyDamage * defenceModifier));
 
   applyDamage(player, modifiedDamage);
